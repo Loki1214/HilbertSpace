0e8edcb059bdeb878d095b538e9c23c495a5c44a
Shoki Sugimoto <45707861+Loki1214@users.noreply.github.com>
2022-10-28 18:31:12 +0900

    Refactored CMakeLists.txt

--- HilbertSpace.hpp
+++ HilbertSpace.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-template<class Derived = int>
-class HilbertSpace;
-
 #ifndef __NVCC__
 	#define __host__
 	#define __device__
 #endif
+
+template<class Derived>
+class HilbertSpace;
 
 template<>
 class HilbertSpace<int> {
@@ -17,53 +17,37 @@ class HilbertSpace<int> {
 		/**
 		 * @brief Default constructor
 		 *
-		 * @param dim Dimension of the Hilbert space to be constructed
+		 * @param dim Dimension of the Hilbert space
 		 */
 		__host__ __device__ HilbertSpace(int dim = 0) : m_dim{dim} {}
-		/**
-		 * @brief Copy constructor
-		 *
-		 * @param other
-		 */
-		__host__ __device__ HilbertSpace(HilbertSpace const& other) = default;
-		/**
-		 * @brief Move constructor
-		 *
-		 * @param other
-		 */
-		__host__ __device__ HilbertSpace(HilbertSpace&& other) = default;
-		/**
-		 * @brief Destructor
-		 *
-		 */
-		__host__ __device__ ~HilbertSpace() = default;
+
+		__host__ __device__               HilbertSpace(HilbertSpace const& other) = default;
+		__host__ __device__ HilbertSpace& operator=(HilbertSpace const& other)    = default;
+		__host__ __device__               HilbertSpace(HilbertSpace&& other)      = default;
+		__host__ __device__ HilbertSpace& operator=(HilbertSpace&& other)         = default;
+		__host__                          __device__ ~HilbertSpace()              = default;
 
 		/*! @name Operator overloads */
 		/* @{ */
-		/**
-		 * @brief Copy assignment operator
-		 *
-		 * @param other
-		 * @return *this
-		 */
-		__host__ __device__ HilbertSpace& operator=(HilbertSpace const& other) = default;
-		/**
-		 * @brief Move assignment operator
-		 *
-		 * @param other
-		 * @return *this
-		 */
-		__host__ __device__ HilbertSpace& operator=(HilbertSpace&& other) = default;
-		/**
-		 * @brief Equality operator
-		 *
-		 * @param other
-		 * @return bool
-		 */
 		__host__ __device__ bool operator==(HilbertSpace const& other) const {
 			return this->dim() == other.dim();
 		}
 		/* @} */
 
 		__host__ __device__ int dim() const { return m_dim; };
+};
+
+template<class Derived>
+class HilbertSpace {
+	public:
+		__host__ __device__ int dim() const {
+			return static_cast<Derived const*>(this)->dim_impl();
+		};
+
+		/*! @name Operator overloads */
+		/* @{ */
+		__host__ __device__ bool operator==(HilbertSpace const& other) const {
+			return this->dim() == other.dim();
+		}
+		/* @} */
 };
