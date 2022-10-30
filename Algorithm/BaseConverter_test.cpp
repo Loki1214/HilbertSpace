449ae56496774b32149a28559528a11ff6f3f398
Shoki Sugimoto <45707861+Loki1214@users.noreply.github.com>
2022-10-30 13:05:52 +0900

    Improved BaseConverter_test.cpp

--- Algorithm/BaseConverter_test.cpp
+++ Algorithm/BaseConverter_test.cpp
@@ -1,12 +1,30 @@
 #include <catch2/catch_test_macros.hpp>
 #include "BaseConverter.hpp"
+#include <iostream>
+#include <random>
 
 template<typename Integer>
 void test_BaseConverter(BaseConverter<Integer> const& bConv, Integer dLoc, Integer L, Integer max) {
 	REQUIRE(dLoc == bConv.radix());
 	REQUIRE(L == bConv.length());
 	REQUIRE(max == bConv.max());
-	for(size_t j = 0; j != bConv.max(); ++j) {
+
+	std::random_device                    seed_gen;
+	std::default_random_engine            engine(seed_gen());
+	std::uniform_int_distribution<size_t> dist(0, bConv.max() - 1);
+	constexpr size_t                      nSample = 100000;
+	Eigen::ArrayX<size_t>                 index;
+	if(nSample >= bConv.max()) {
+		index.resize(bConv.max());
+		for(size_t j = 0; j != bConv.max(); ++j) index(j) = j;
+	}
+	else {
+		index = index.NullaryExpr(nSample, [&]() { return dist(engine); });
+	}
+
+#pragma omp parallel for
+	for(size_t sample = 0; sample != static_cast<size_t>(index.size()); ++sample) {
+		auto j      = index(sample);
 		auto config = bConv.number_to_config(j);
 		REQUIRE(j == bConv.config_to_number(config));
 		for(size_t pos = 0; pos != bConv.length(); ++pos) {
@@ -32,6 +50,15 @@ TEST_CASE("BaseConverter", "test") {
 	{
 		size_t dLoc = 2;
 		for(size_t L = 0; L <= LMax; ++L) {
+			std::cout << "dloc = " << dLoc << ", L = " << L << std::endl;
+			BaseConverter<size_t> bConv(dLoc, L);
+			test_BaseConverter(bConv, dLoc, L, powi(dLoc, L));
+		}
+	}
+	{
+		size_t dLoc = 4;
+		for(size_t L = 0; L <= 16; ++L) {
+			std::cout << "dloc = " << dLoc << ", L = " << L << std::endl;
 			BaseConverter<size_t> bConv(dLoc, L);
 			test_BaseConverter(bConv, dLoc, L, powi(dLoc, L));
 		}
