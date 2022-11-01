6e7a35abe79d24085ecfc85f8ca24fa636437fe1
Shoki Sugimoto <45707861+Loki1214@users.noreply.github.com>
2022-11-01 20:57:56 +0900

    Introduced find_package(Eigen3) for all CMakeLists.txt

--- OpSpace/mBodyOpSpace_test.cpp
+++ OpSpace/mBodyOpSpace_test.cpp
@@ -32,6 +32,11 @@ TEST_CASE("ManyBodyOpSpace", "test") {
 			for(size_t m = 1; m <= sysSize; ++m) {
 				std::cout << "sysSize = " << sysSize << ", m = " << m << std::endl;
 				mBodyOpSpace<decltype(mbSpace), Scalar> opSpace(m, mbSpace);
+				if(m > 4 && opSpace.dim() > 100000000) {
+					std::cout << "opSpace.dim() = "
+					          << opSpace.dim() " is so large. Skipping test..." << std::endl;
+					continue;
+				}
 				test_ManyBodySpaceBase(opSpace, sysSize, OpSpace<Scalar>(locSpace));
 				test_OpSpace(opSpace);
 			}
